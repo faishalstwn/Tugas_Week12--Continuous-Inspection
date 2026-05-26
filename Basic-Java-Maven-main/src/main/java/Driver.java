f639734c08d2a8e044cea1bde24af79d9f496e7f
faishalstwn <169064956+faishalstwn@users.noreply.github.com>
2026-05-26 20:14:12 +0700

    Update Driver.java

--- Basic-Java-Maven-main/src/main/java/Driver.java
+++ Basic-Java-Maven-main/src/main/java/Driver.java
@@ -1,21 +1,20 @@
-import src.main.java.Counter;
-
+//test sonar
 public class Driver {
 
 	public static void main(String[] args) {
-		
+
 		Counter counter = new Counter();
-		
+
 		System.out.println("Current count:" + counter.getCount());
-		
+
 		counter.increment();
-		
+
 		System.out.println("Current count:" + counter.getCount());
-		
+
 		counter.decrement();
-		
+
 		System.out.println("Current count:" + counter.getCount());
-		
+
 	}
 
 }
